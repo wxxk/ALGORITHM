496a0eb190aa5395050beb593b5e8fb7db566233
wxxk <dwde22@gmail.com>
2023-01-09 22:46:46 +0900

    백준 피보나치 수3 풀이

---
+++ BOJ/2749. 피보나치 수 3/BOJ_2749.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.stdin = open("input.txt")
+
+n = int(input())
+mod = 1000000
+fibo = [0, 1]
+p = mod // 10 * 15
+
+for i in range(2, p):
+    fibo.append(fibo[i - 1] + fibo[i - 2])
+    fibo[i] %= mod
+
+print(fibo[n % p])
