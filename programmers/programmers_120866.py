afe73c76bb4b25c465e13c4574aa0c33462edb86
wxxk <dwde22@gmail.com>
2023-01-17 11:04:19 +0900

    프로그래머스 안전지대 풀이

---
+++ programmers/programmers_120866.py
@@ -0,0 +1,34 @@
+from pprint import pprint
+
+board = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 0, 0, 0, 0],
+]
+n = len(board)
+visited = [[False] * (n) for _ in range(n)]
+dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+dy = [0, 0, -1, 1, -1, 1, -1, 1]
+stack = []
+cnt = 0
+
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1 and not visited[i][j]:
+            visited[i][j] = True
+            stack.append((i, j))
+
+for x, y in stack:
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n:
+            board[nx][ny] = 1
+
+for a in range(n):
+    for b in range(n):
+        if board[a][b] == 0:
+            cnt += 1
+print(cnt)
