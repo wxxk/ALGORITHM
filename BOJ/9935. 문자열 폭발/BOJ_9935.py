73f1c121fa7db6d0118981b4764830d29578a83e
wxxk <dwde22@gmail.com>
2023-01-17 09:54:20 +0900

    백준 문자열 폭발 풀이

--- BOJ/9935. 문자열 폭발/BOJ_9935.py
+++ BOJ/9935. 문자열 폭발/BOJ_9935.py
@@ -3,7 +3,18 @@ import sys
 sys.stdin = open("input.txt")
 input = sys.stdin.readline
 
-word = list(input().rstrip())
-bomb = list(input())
+word = input().strip()
+bomb = list(input().strip())
 
 result = []
+
+for i in word:
+    result.append(i)
+    if result[-len(bomb) :] == bomb:
+        for _ in range(len(bomb)):
+            result.pop()
+
+if result:
+    print("".join(result))
+else:
+    print("FRULA")
