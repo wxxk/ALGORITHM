bfb41e0c0885bf4c8e50c2ec1e26f089a6de43ce
wxxk <dwde22@gmail.com>
2023-01-21 15:46:49 +0900

    백준 이모티콘 풀이

---
+++ BOJ/14226. 이모티콘/BOJ_14226.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+sys.stdin = open("input.txt")
+
+S = int(input())
+visited = [[-1] * (S + 1) for _ in range(S + 1)]
+visited[1][0] = 1
+
+queue = deque()
+queue.append((1, 0, 0))
+
+while queue:
+    now, clip, time = queue.popleft()
+
+    if now == S:
+        print(time)
+        break
+
+    # 1번 조건 : 복사
+    if visited[now][now] == -1:
+        visited[now][now] = visited[now][clip] + 1
+        queue.append((now, now, time + 1))
+
+    if 2 <= now + clip < S + 1 and visited[now + clip][clip] == -1:
+        visited[now + clip][clip] = visited[now][clip] + 1
+        queue.append((now + clip, clip, time + 1))
+
+    if 2 <= now - 1 < S + 1 and visited[now - 1][clip] == -1:
+        visited[now - 1][clip] = visited[now][clip] + 1
+        queue.append((now - 1, clip, time + 1))
