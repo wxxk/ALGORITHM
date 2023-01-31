4718dc4331013c0e9cb43851af71c5e8e8efbd0f
wxxk <dwde22@gmail.com>
2023-01-31 11:16:31 +0900

    프로그래머스 뒤에 있는 큰 수 풀이

--- programmers/programmers_154539.py
+++ programmers/programmers_154539.py
@@ -1,5 +1,12 @@
 numbers = [2, 3, 3, 5]
+answer = [0] * len(numbers)
 stack = []
 
-for i in range(1, len(numbers)):
-    stack.append(numbers[i])
+for i in range(len(numbers)):
+    while stack and numbers[stack[-1]] < numbers[i]:
+        answer[stack.pop()] = numbers[i]
+    stack.append(i)
+while stack:
+    answer[stack.pop()] = -1
+
+print(answer)
