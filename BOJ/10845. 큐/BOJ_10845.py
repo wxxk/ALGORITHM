f83fe1be93d41e03b12cea2c12e5269315bde4ee
wxxk <dwde22@gmail.com>
2023-02-06 11:05:26 +0900

    백준 10845 풀이

---
+++ BOJ/10845. 큐/BOJ_10845.py
@@ -0,0 +1,42 @@
+from collections import deque
+import sys
+
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+n = int(input())
+queue = deque()
+
+
+for i in range(n):
+    com = input().split()
+
+    if com[0] == "push":
+        queue.append(int(com[1]))
+
+    elif com[0] == "pop":
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+
+    elif com[0] == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+
+    elif com[0] == "back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
+
+    elif com[0] == "size":
+        print(len(queue))
+
+    elif com[0] == "empty":
+        if queue:
+            print(0)
+        else:
+            print(1)
