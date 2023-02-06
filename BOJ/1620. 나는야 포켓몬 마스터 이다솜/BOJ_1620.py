4a599a772c2109607aba0f6330611be38ff07e9a
wxxk <dwde22@gmail.com>
2023-02-06 11:23:32 +0900

    백준 나는야 포켓몬 마스터 이다솜 풀이

--- BOJ/1620. 나는야 포켓몬 마스터 이다솜/BOJ_1620.py
+++ BOJ/1620. 나는야 포켓몬 마스터 이다솜/BOJ_1620.py
@@ -1,13 +1,19 @@
 import sys
-sys.stdin = open('input.txt')
+
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 dict_1 = {}
 
-for i in range(1, n+1):
-    word = input()
-    dict_1[str(i)] = word
+for i in range(1, n + 1):
+    word = input().strip()
+    dict_1[i] = word
     dict_1[word] = i
 
 for i in range(m):
-    print(dict_1[input()])
+    quest = input().strip()
+    if quest.isdigit():
+        print(dict_1[int(quest)])
+    else:
+        print(dict_1[quest])
