95de5823fed9689429c090493f5ee35c06d600a7
wxxk <dwde22@gmail.com>
2023-03-02 01:52:06 +0900

    프로그래머스 피자 나눠 먹기 (3) 풀이

---
+++ programmers/120816. 피자 나눠 먹기 (3)/programmers_120816.py
@@ -0,0 +1,16 @@
+slice = 5
+n = 7
+
+
+def solution(slice, n):
+    answer = 0
+    if n % slice == 0:
+        answer = n // slice
+
+    else:
+        answer = (n // slice) + 1
+
+    return answer
+
+
+print(solution(slice, n))
