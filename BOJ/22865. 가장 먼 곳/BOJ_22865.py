180bd6404bc81d27df7ae9caffc334b61dbbe81a
wxxk <dwde22@gmail.com>
2023-03-02 09:55:31 +0900

    백준 가장 먼 곳 풀이

---
+++ BOJ/22865. 가장 먼 곳/BOJ_22865.py
@@ -0,0 +1,39 @@
+from heapq import heappush, heappop
+import sys
+
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
+INF = sys.maxsize
+
+n = int(input())
+friend = list(map(int, input().split()))  # a, b, c 가 사는 위치
+m = int(input())
+maps = [[] for _ in range(n + 1)]
+for _ in range(m):
+    d, e, l = map(int, input().split())
+    maps[d].append((e, l))
+    maps[e].append((d, l))
+visited = [INF] * (n + 1)
+q = []
+for i in friend:
+    heappush(q, (0, i))
+
+while q:
+    cost, node = heappop(q)
+
+    for i, j in maps[node]:
+        nx = cost + j
+
+        if visited[i] > nx:
+            visited[i] = nx
+            heappush(q, (nx, i))
+
+temp = 0
+answer = 0
+
+for i in range(1, len(visited)):
+    if visited[i] > temp:
+        temp = visited[i]
+        answer = i
+
+print(answer)
