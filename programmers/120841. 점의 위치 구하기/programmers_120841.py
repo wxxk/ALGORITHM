62d2de0fc1c56e626cb4f4d9c1edf3e14f191d73
wxxk <dwde22@gmail.com>
2023-03-03 11:35:33 +0900

    프로그래머스 점의 위치 구하기 풀이

---
+++ programmers/120841. 점의 위치 구하기/programmers_120841.py
@@ -0,0 +1,19 @@
+dot = [-7, 9]
+
+
+def solution(dot):
+    answer = 0
+
+    if dot[0] > 0 and dot[1] > 0:
+        answer = 1
+    elif dot[0] < 0 and dot[1] > 0:
+        answer = 2
+    elif dot[0] < 0 and dot[1] < 0:
+        answer = 3
+    elif dot[0] > 0 and dot[1] < 0:
+        answer = 4
+
+    return answer
+
+
+print(solution(dot))
