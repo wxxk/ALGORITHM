2219202de417bcde649822059f9e5fd539cf8165
wxxk <dwde22@gmail.com>
2023-03-07 10:38:48 +0900

    프로그래머스 중복된 문자 제거 풀이

---
+++ programmers/120888. 중복된 문자 제거/programmers_120888.py
@@ -0,0 +1,14 @@
+def solution(my_string):
+    answer = ""
+    for i in my_string:
+        if i not in answer:
+            answer += i
+
+        else:
+            continue
+    return answer
+
+
+my_string = "people"
+
+print(solution(my_string))
