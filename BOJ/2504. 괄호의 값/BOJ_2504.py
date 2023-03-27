2ae5d94b94517519c255a1eb3bd489ff7ce07e97
wxxk <dwde22@gmail.com>
2023-03-27 22:14:51 +0900

    백준 괄호의 값 풀이

---
+++ BOJ/2504. 괄호의 값/BOJ_2504.py
@@ -0,0 +1,42 @@
+import sys
+
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+back = list(input())
+stack = []
+temp = 1
+answer = 0
+
+for i in range(len(back)):
+    if back[i] == "(":
+        stack.append(back[i])
+        temp *= 2
+
+    elif back[i] == "[":
+        stack.append(back[i])
+        temp *= 3
+
+    elif back[i] == ")":
+        if not stack or stack[-1] == "[":
+            answer = 0
+            break
+        if back[i - 1] == "(":
+            answer += temp
+        stack.pop()
+        temp //= 2
+
+    else:
+        if not stack or stack[-1] == "(":
+            answer = 0
+            break
+        if back[i - 1] == "[":
+            answer += temp
+
+        stack.pop()
+        temp //= 3
+
+if stack:
+    print(0)
+else:
+    print(answer)
