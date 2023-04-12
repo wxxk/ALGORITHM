b1fc3787e40221ff5f4803cb484eac8d245913cb
wxxk <dwde22@gmail.com>
2023-04-12 22:19:40 +0900

    백준 RGB거리 풀이

---
+++ BOJ/1149. RGB거리/BOJ_1149.py
@@ -0,0 +1,15 @@
+import sys
+
+# sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+n = int(input())
+dp = []
+
+for i in range(n):
+    dp.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + dp[i][0]
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + dp[i][1]
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + dp[i][2]
