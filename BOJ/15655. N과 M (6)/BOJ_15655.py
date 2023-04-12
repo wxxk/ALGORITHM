b3b0ec106f7b8d637b67a7c4346d6f1ec4bbf5ab
wxxk <dwde22@gmail.com>
2023-04-12 22:21:45 +0900

    백준 n과 m(6) 풀이

---
+++ BOJ/15655. N과 M (6)/BOJ_15655.py
@@ -0,0 +1,26 @@
+import sys
+
+# sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+
+def back(start):
+    if len(res) == m:
+        print(*res)
+        return
+
+    for i in range(start, n + 1):
+        if not visited[i]:
+            visited[i] = True
+            res.append(num[i - 1])
+            back(i + 1)
+            visited[i] = False
+            res.pop()
+
+
+n, m = map(int, input().split())
+num = sorted(list(map(int, input().split())))
+visited = [False] * (n + 1)
+res = []
+
+back(1)
