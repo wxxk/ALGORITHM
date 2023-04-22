4bfe1fd25723b88cf18fe99ffd46a26a8695d4ee
wxxk <dwde22@gmail.com>
2023-04-22 22:20:43 +0900

    백준 이친수 풀이

---
+++ BOJ/2193. 이친수/BOJ_2193.py
@@ -0,0 +1,18 @@
+import sys
+
+# sys.stdin = open("input.txt")
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0 for _ in range(n + 1)]
+
+dp[1] = 1
+
+if n >= 2:
+    dp[2] = 1
+
+for i in range(3, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[n])
