9563cf9c8a9461a3fda1a8acb81d2d887de42c32
wxxk <dwde22@gmail.com>
2023-04-30 16:17:42 +0900

    백준 설탕배달 수정

--- BOJ/2839. 설탕 배달/BOJ_2839.py
+++ BOJ/2839. 설탕 배달/BOJ_2839.py
@@ -1,16 +1,17 @@
 import sys
 
-# sys.stdin = open("input.txt")
+sys.stdin = open("input.txt")
 input = sys.stdin.readline
 
 n = int(input())
 cnt = 0
 
-while n >= 0:
+while n > 0:
     if n % 5 == 0:
         cnt += n // 5
         print(cnt)
         break
+
     n -= 3
     cnt += 1
 
